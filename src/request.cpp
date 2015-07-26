f9fd9118db29c5b2d357efd4034e3ee926690f84
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2015-07-27 00:32:01 +0900

    split request line

--- src/request.cpp
+++ src/request.cpp
@@ -1,6 +1,19 @@
 #include <rapid/rapid.h>
+#include <sstream>
+#include <vector>
 
 namespace rapid {
 
-Request::Request(const std::string &raw_input) {}
+std::vector<std::string> split(const std::string &input) {
+  std::istringstream stream(input);
+  std::string result;
+  std::vector<std::string> results;
+  while (std::getline(stream, result, '\n')) {
+    result.erase(result.size() - 1);
+    results.emplace_back(result);
+  }
+  return std::move(results);
+}
+
+Request::Request(const std::string &raw_input) { auto rows = split(raw_input); }
 }
