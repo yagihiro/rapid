6e8e7db18cdd65cac2535d6cfe522346e8bb031b
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2016-03-29 01:09:23 +0900

    fix build error on gcc

--- include/rapid/peer.h
+++ include/rapid/peer.h
@@ -6,7 +6,7 @@ namespace rapid {
 /**
  Client and Server address/port pairs
  */
-using Peer = struct {
+using Peer = struct _Peer {
   std::string client_addr;
   uint16_t client_port;
   std::string server_addr;
